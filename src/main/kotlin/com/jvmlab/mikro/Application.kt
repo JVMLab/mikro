f13063c9bd55f9cb23b08094afbc2fdb3cff8e23
Denis Korchuganov <dkorchuganov@yandex.ru>
2022-06-18 01:03:14 +0300

    Switched to EngineMain and added the first test

--- src/main/kotlin/com/jvmlab/mikro/Application.kt
+++ src/main/kotlin/com/jvmlab/mikro/Application.kt
@@ -1,24 +1,44 @@
 package com.jvmlab.mikro
 
-import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import com.jvmlab.mikro.plugins.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
 import kotlinx.serialization.json.Json
+import io.ktor.server.routing.*
 
 
-fun main() {
-    embeddedServer(Netty, port = 8080) {
-        install(ContentNegotiation) {
-            json(
-                Json {
-                    prettyPrint = true
-                    encodeDefaults = true
-                }
-            )
-        }
-        configureRouting()
-    }.start(wait = true)
+fun main(args: Array<String>): Unit = EngineMain.main(args)
+
+
+fun Application.module() {
+
+  install(ContentNegotiation) {
+    json(
+      Json {
+        prettyPrint = true
+        encodeDefaults = true
+      }
+    )
+  }
+
+  routing {
+    get("/") {
+      call.respondText("Hello, World!")
+    }
+
+    route("/guid") {
+      get {
+        call.respond(GuidParams().guid())
+      }
+
+      post {
+        val params = call.receive<GuidParams>()
+        call.respond(params.guid())
+      }
+    }
+  }
 }
+
--- src/test/kotlin/com/jvmlab/mikro/ApplicationTest.kt
+++ src/test/kotlin/com/jvmlab/mikro/ApplicationTest.kt
@@ -7,7 +7,16 @@ import io.ktor.server.response.*
 import io.ktor.server.request.*
 import kotlin.test.*
 import io.ktor.server.testing.*
-import com.jvmlab.mikro.plugins.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+
 
 class ApplicationTest {
+
+  @Test
+  fun testRoot() = testApplication {
+    val response = client.get("/")
+    assertEquals(HttpStatusCode.OK, response.status)
+    assertEquals("Hello, World!", response.bodyAsText())
+  }
 }
